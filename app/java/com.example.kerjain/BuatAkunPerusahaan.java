376bb329344811268f3b0a110db34a89c2c58112
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-03-18 23:05:27 +0700

    Create BuatAkunPerusahaan.java

---
+++ app/java/com.example.kerjain/BuatAkunPerusahaan.java
@@ -0,0 +1,13 @@
+package com.example.kerjain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuatAkunPerusahaan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_akun_perusahaan);
+    }
+}
